e1f591703bfcf26e91d8e3979d6aa5297db38040
Pankesh Patel <pankeshlinux@gmail.com>
2018-05-20 01:11:42 -0400

    changed mqtt broker changed
    Changed the publically available broker "mosquitios" from local broker. This is because the new users may have to do extra work to set up a mqtt broker locally, just to test the code

--- samples/mqtt_consumer.py
+++ samples/mqtt_consumer.py
@@ -10,7 +10,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     # client.subscribe("$SYS/#")
-    client.subscribe("samples")
+    client.subscribe("sample")
 
 
 # The callback for when a PUBLISH message is received from the server.
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("localhost", 1883, 60)
+    client.connect("test.mosquitto.org", 1883, 60)
 
     # Blocking call that processes network traffic, dispatches
     # callbacks and handles reconnecting.  Other loop*() functions are
